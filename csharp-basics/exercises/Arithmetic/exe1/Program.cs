de411490dfa3e89581eb84052303c0f023b69e10
Humigan <woodoteam@gmail.com>
2021-07-18 22:41:59 +0300

    Arithmetic exercises

--- csharp-basics/exercises/Arithmetic/exe1/Program.cs
+++ csharp-basics/exercises/Arithmetic/exe1/Program.cs
@@ -4,25 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace exee3
+namespace ConsoleApp3
 {
     class Program
     {
         static void Main(string[] args)
         {
             int a, b;
-            int sum = 0;
-            int count = 0;
             a = int.Parse(Console.ReadLine());
             b = int.Parse(Console.ReadLine());
-            for (int i = a; i < b + 1; i++)
-            {
-                sum = sum + i;
-                count++;
-            }
-            Console.WriteLine(sum);
+            bool Either = false;
 
-            Console.WriteLine(sum / count);
+            if (a == 15 || b == 15 || a + b == 15 || a - b == 15 || b - a == 15)
+            {
+                Either = true;
+            }
+            Console.WriteLine(Either);
         }
     }
 }
--- csharp-basics/exercises/Arithmetic/exe4/Program.cs
+++ csharp-basics/exercises/Arithmetic/exe4/Program.cs
@@ -4,25 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace exee3
+namespace exe3
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int a, b;
-            int sum = 0;
-            int count = 0;
+            double a, b, result = 0;
+            double count = 0;
+            double ave;
             a = int.Parse(Console.ReadLine());
             b = int.Parse(Console.ReadLine());
-            for (int i = a; i < b + 1; i++)
+            for (double i = a; i < b + 1; i++)
             {
-                sum = sum + i;
+                result = result + i;
                 count++;
             }
-            Console.WriteLine(sum);
-
-            Console.WriteLine(sum / count);
+            ave = result / count;
+            Console.WriteLine(result);
+            Console.WriteLine((double)ave);
         }
     }
 }
