f6defa5efe38afefa59daf8edc05d8625c380ff7
Humigan <woodoteam@gmail.com>
2021-07-19 10:07:58 +0300

    Arrays Exercises

--- csharp-basics/exercises/Arrays/Arrays/Exercise4/Program.cs
+++ csharp-basics/exercises/Arrays/Arrays/Exercise4/Program.cs
@@ -17,14 +17,16 @@ namespace Exercise4
                 1458, 2458, 1254, 1472, 2365,
                 1456, 2265, 1457, 2456
             };
-
-            /*
-            fixme - check if contains number 1245 
-            for (?) {
-                ......
-                Console.WriteLine("Contains!");
+            Console.WriteLine("If an array contains a specific value");
+            int specific = int.Parse(Console.ReadLine());
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                if (myArray[i] == specific)
+                {
+                    Console.WriteLine("Contains!");
+                    
+                }
             }
-            */
         }
     }
 }
