de9363dfcdf9c9bc02ae1924d85336fc429d0c35
Humigan <woodoteam@gmail.com>
2021-08-13 14:27:41 +0300

    collection fix

---
+++ csharp-basics/exercises/Polymorphism/Persons/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persons
+{
+    class Employee : Person
+    {
+        private string jobTitle;
+
+        public Employee(string jobTitle)
+        {
+            this.jobTitle = jobTitle;
+        }
+
+        public string _jobTitle
+        {
+            get { return jobTitle; }
+            set { jobTitle = value; }
+        }
+
+        public override void display()
+        {
+            base.display();
+            Console.WriteLine("Job Title = " + jobTitle);
+        }
+    }
+}
--- csharp-basics/exercises/Polymorphism/Persons/Program.cs
+++ csharp-basics/exercises/Polymorphism/Persons/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace Persons
 {
@@ -6,10 +8,28 @@ namespace Persons
     {
         static void Main(string[] args)
         {
-            /*
-             * - Create an instant of class Student and an instant of class Employee with proper values for the attributes.
-             * - Display the content of each class using Display() method.
-             */
+            var ppl = new Person();
+            ppl._firstName = "Stas";
+            ppl._lastName = "Boreckis";
+            ppl._address = "Nicgales 40";
+            ppl._id = 1115;
+            ppl.display();
+
+            var stud = new Student(20.5);
+            stud._firstName = "Andrejs";
+            stud._lastName = "Akels";
+            stud._address = "Saharova 10";
+            stud._id = 999;
+            stud._gpa = 35.8;
+            stud.display();
+
+            var emp = new Employee("Taxi");
+            emp._firstName = "Nikita";
+            emp._lastName = "Tumens";
+            emp._address = "Ilukstes 28";
+            emp._id = 23;
+            emp._jobTitle = "Teacher";
+            emp.display();
         }
     }
 }
